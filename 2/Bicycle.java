1d661b2c2ae4c082cf4cbe157bd3b0db1773cefb
goida911 <131038197+belisemo@users.noreply.github.com>
2026-05-25 15:09:52 +0300

    Create Bicycle.java

---
+++ 2/Bicycle.java
@@ -0,0 +1,33 @@
+public abstract class Bicycle {
+
+    protected String brand;
+    protected String type;
+    protected int weight;
+
+    protected static int count = 0;
+    
+    public Bicycle(String brand, String type, int weight) {
+        this.brand = brand;
+        this.type = type;
+        this.weight = weight;
+        count++;
+    }
+
+    public Bicycle() {
+        this("Без названия", "Неизвестно", 0);
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+
+    public abstract void showInfo();
+}

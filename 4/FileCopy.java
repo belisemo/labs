76bfc4cfa43c818d62714d2149fe2397fc14748d
goida911 <131038197+belisemo@users.noreply.github.com>
2026-05-29 16:58:10 +0300

    Create FileCopy.java

---
+++ 4/FileCopy.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class FileCopy {
+    public static void main(String[] args) {
+        FileInputStream input = null;
+        FileOutputStream output = null;
+
+        try {
+            input = new FileInputStream("C:/Users/belis/IdeaProjects/laba4/source.txt");
+            output = new FileOutputStream("sourceb.txt");
+
+            int n;
+            while ((n = input.read()) != -1) {
+                output.write(n);
+            }
+
+            System.out.println("Файл успешно скопирован!");
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при работе с файлом: " + e.getMessage());
+        } finally {
+            try {
+                if (input != null) input.close();
+                if (output != null) output.close();
+            } catch (IOException e) {
+                System.out.println("Не удалось закрыть файлы");
+            }
+        }
+    }
+}
